0f3de3138b8daf6a924cd5c3260e88603e9c53fb
giankan34 <giankan34@gmail.com>
2026-05-31 12:16:10 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,112 +1,11 @@
-const releaseContainer =
-document.getElementById(
-    "releaseContainer"
-);
+function loginWithPi(){
 
-async function loadReleases(){
+    const screen =
+    document.getElementById(
+        "screenText"
+    );
 
-    try{
-
-        const response =
-        await fetch(
-            './data/releases.json'
-        );
-
-        const releases =
-        await response.json();
-
-        releaseContainer.innerHTML = "";
-
-        releases.forEach(
-        (release)=>{
-
-            const card =
-            document.createElement(
-                "div"
-            );
-
-            card.className =
-            "release-card";
-
-            card.innerHTML = `
-
-                <img
-                class="release-cover"
-                src="${release.cover}"
-                alt="${release.release}">
-
-                <div class="release-title">
-
-                    ${release.release}
-
-                </div>
-
-                <div class="release-artist">
-
-                    ${release.artist}
-
-                </div>
-
-                <div class="release-desc">
-
-                    ${release.genre}
-                    •
-                    ${release.year}
-
-                </div>
-
-                <div class="release-desc">
-
-                    ${release.description}
-
-                </div>
-
-                ${
-                    release.bandcamp
-                    ?
-                    `
-                    <div class="bandcamp-frame">
-
-                        <iframe
-                        style="height:120px;"
-                        src="${release.bandcamp}"
-                        seamless>
-
-                        </iframe>
-
-                    </div>
-                    `
-                    :
-                    `
-                    <audio
-                    class="audio-player"
-                    controls>
-
-                        <source
-                        src="${release.audio}"
-                        type="audio/mpeg">
-
-                    </audio>
-                    `
-                }
-
-            `;
-
-            releaseContainer.appendChild(
-                card
-            );
-
-        });
-
-    }catch(error){
-
-        console.error(
-            "Release loading error:",
-            error
-        );
-
-    }
+    screen.innerText =
+    "PI LOGIN COMING SOON";
 
 }
-
-loadReleases();
