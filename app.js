814b8b9e730e37d5700684618ca5ae29ea43743e
giankan34 <giankan34@gmail.com>
2026-06-11 12:06:30 +0300

    Update app.js

--- app.js
+++ app.js
@@ -9,3 +9,71 @@ function loginWithPi(){
     "PI LOGIN COMING SOON";
 
 }
+const searchInput =
+document.getElementById(
+    "searchInput"
+);
+
+if(searchInput){
+
+searchInput.addEventListener(
+"input",
+async function(){
+
+const query =
+this.value.toLowerCase();
+
+const resultsBox =
+document.getElementById(
+"searchResults"
+);
+
+if(query.length < 2){
+
+resultsBox.innerHTML = "";
+
+return;
+
+}
+
+const response =
+await fetch(
+"./data/releases.json"
+);
+
+const releases =
+await response.json();
+
+const matches =
+releases.filter(item =>
+
+item.artist
+.toLowerCase()
+.includes(query)
+
+||
+
+item.release
+.toLowerCase()
+.includes(query)
+
+);
+
+resultsBox.innerHTML =
+
+matches.map((item,index)=>`
+
+<a
+href="release.html?id=${index}"
+class="similar-link">
+
+${item.artist}
+<br>
+<small>${item.release}</small>
+
+</a>
+
+`).join("");
+
+});
+}
