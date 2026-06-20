4c4aa3026e1107bf316443710e0cb07cf6b492c3
giankan34 <giankan34@gmail.com>
2026-06-20 20:21:06 +0300

    Update app.js

--- app.js
+++ app.js
@@ -338,6 +338,55 @@ ${nextRank}
 </span>
 
 `;
+    if(secretBox){
+
+    const secrets = [];
+
+    if(collection.length >= 3){
+
+        secrets.push(
+            "📀 COLLECTOR"
+        );
+
+    }
+
+    if(collection.length >= 10){
+
+        secrets.push(
+            "⚔ OSSVARIUM DEVOTEE"
+        );
+
+    }
+
+    if(collection.length >= 20){
+
+        secrets.push(
+            "🏛 KEEPER OF THE TOMBS"
+        );
+
+    }
+
+    secretBox.innerHTML =
+
+    secrets.length > 0
+
+    ?
+
+    `
+
+    <b>☠ SECRET ACHIEVEMENTS ☠</b>
+
+    <br><br>
+
+    ${secrets.join("<br><br>")}
+
+    `
+
+    :
+
+    "";
+
+    }
 }
     
     if(collection.length === 0){
