911dbb9723ff7a2c35c520ddc463278b662a2b43
giankan34 <giankan34@gmail.com>
2026-06-23 19:00:50 +0300

    Update app.js

--- app.js
+++ app.js
@@ -434,7 +434,7 @@ if(countriesCollected.size >= 10){
 
     }
 }
-
+/*
  if(relicBox){
 
     const relics = [];
@@ -480,7 +480,7 @@ if(countriesCollected.size >= 10){
     "";
 
         }
-    
+    */
     if(collection.length === 0){
 
     title.innerHTML =
